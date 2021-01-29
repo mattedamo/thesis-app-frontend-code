e4bbe67731ec83d6c9e246df155f23b10d6835f8
mattedamo <mattedamo@gmail.com>
2021-01-29 12:17:15 +0100

    a

---
+++ set-ns.py
@@ -0,0 +1,27 @@
+
+import yaml, os
+
+
+def main():
+    github_ref = os.getenv("GITHUB_REF")
+    ref_list = github_ref.split("/")
+    if "master" in github_ref:
+        branch = ref_list[-1]
+        ns = "prod"
+    elif "releases" or "features" in github_ref:
+        ns = ref_list[-2]+"/"+ref_list[-1]
+    else:
+        raise Exception("Not valid branch name")
+
+    with open("./input.yaml") as file:
+        input = yaml.load(file, Loader=yaml.FullLoader)
+    if "ns" in input.keys():
+        if ns != input["ns"]:
+            raise Exception("You have to edit input.yaml before expect that it can be applied on kubernetes!")
+    else:
+        input["ns"] = ns
+    with open("./input.yaml", "w") as file:
+                yaml.dump(input, file)
+
+if __name__ == '__main__':
+    main()
---
+++ validation-input-file.py
@@ -0,0 +1,22 @@
+import yaml, os
+
+def main():
+    github_ref = os.environ["GITHUB_REF"]
+    if "master" in github_ref:
+        branch = "master"
+    elif "features" or "releases" in github_ref:
+        branch_list = github_ref.split("/")
+        branch = branch_list[-2]+"/"+branch_list[-1]
+    with open("./input.yaml") as file:
+        input = yaml.load(file, Loader=yaml.FullLoader)
+    if "branch" not in input.keys():
+        raise Exception("You have to insert the correct namespace manually into the input yaml file, "+ 
+                        "to confirm that you have made the actual changes in it!")
+    if branch != input["branch"]:
+        raise Exception("You have to insert the correct namespace manually into the input yaml file, "+ 
+                        "to confirm that you have made the actual changes in it!")
+       
+    if "clusters" not in input.keys():
+        raise Exception("You have to specific the cluster name(s) where you want to deploy the application!")
+if __name__ == '__main__':
+    main()
